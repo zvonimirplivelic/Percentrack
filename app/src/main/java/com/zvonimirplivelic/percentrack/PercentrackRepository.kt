1d070c36c29bce3a1081279c26536a764b22bd0c
Zvonimir <zvonimirplivelic91@gmail.com>
2022-05-05 18:59:58 +0200

    Creating viewmodel and repository finished

---
+++ app/src/main/java/com/zvonimirplivelic/percentrack/PercentrackRepository.kt
@@ -0,0 +1,22 @@
+package com.zvonimirplivelic.percentrack
+
+import androidx.lifecycle.LiveData
+import com.zvonimirplivelic.percentrack.db.PercentrackDao
+import com.zvonimirplivelic.percentrack.model.Exercise
+
+class PercentrackRepository(private val percentrackDao: PercentrackDao) {
+
+    val getExerciseList: LiveData<List<Exercise>> = percentrackDao.getExerciseList()
+
+    suspend fun addExercise(exercise: Exercise) {
+        percentrackDao.addExercise(exercise)
+    }
+
+    suspend fun updateExercise(exercise: Exercise) {
+        percentrackDao.updateExercise(exercise)
+    }
+
+    suspend fun deleteExercise(exercise: Exercise) {
+
+    }
+}
---
+++ app/src/main/java/com/zvonimirplivelic/percentrack/PercentrackViewModel.kt
@@ -0,0 +1,40 @@
+package com.zvonimirplivelic.percentrack
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.zvonimirplivelic.percentrack.db.PercentrackDatabase
+import com.zvonimirplivelic.percentrack.model.Exercise
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PercentrackViewModel(application: Application) : AndroidViewModel(application) {
+
+    val getExerciseList: LiveData<List<Exercise>>
+    private val repository: PercentrackRepository
+
+    init {
+        val percentrackDao = PercentrackDatabase.invoke(application).getPercentrackDao()
+        repository = PercentrackRepository(percentrackDao)
+        getExerciseList = repository.getExerciseList
+    }
+
+    fun addExercise(exercise: Exercise) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addExercise(exercise)
+        }
+    }
+
+    fun updateExercise(exercise: Exercise) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateExercise(exercise)
+        }
+    }
+
+    fun deleteExercise(exercise: Exercise) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteExercise(exercise)
+        }
+    }
+}
